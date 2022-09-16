b4cede6ba908e8d141ff95a21293bf35a047541f
ManoShres <manojrillmark@gmail.com>
2022-09-16 14:31:51 +0545

    ClassNObjectExample

--- ClassNObjet.py
+++ ClassNObjet.py
@@ -1,4 +1,5 @@
 from tkinter import Variable
+from unicodedata import name
 
 
 class Klass:
@@ -34,3 +35,27 @@ class NumberHolder:
 
 var = NumberHolder(7)
 print(var.returnNumber()) #Prints '7'
+
+### Exercise
+# define the Vehicle class
+class Vehicle:
+    name = ""
+    kind = "car"
+    color = ""
+    value = 100.00
+    def description(self):
+        desc_str = "%s is a %s %s worth $%.2f." % (self.name, self.color, self.kind, self.value)
+        return desc_str     
+# your code goes here
+car1 = Vehicle()
+car1.name = "Toyata"
+car1.color = "Red"
+car1.value = 60000
+
+car2 = Vehicle()
+car2.name = "Tesla"
+car2.color = "Red"
+car2.value = 160000
+# test code
+print(car1.description())
+print(car2.description())
